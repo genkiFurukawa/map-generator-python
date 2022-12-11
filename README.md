84177ef2ecff8f0ac73355d807fd8268db373644
GENKI FURUKAWA <genki.furukawa@gmail.com>
2022-12-11 15:24:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python で国土地理院のデータを使って地図を生成する
+
+## バージョン
+
+```
+$ python -V
+Python 3.10.9
+```
+
+## 参考
+
+### 環境構築
+
+- https://kokiblog.com/2022/01/20/anyenv-pyenv-pipenv/
+
+### プロジェクト構成など
+
+- https://rinatz.github.io/python-book/
+
+### 画像処理ライブラリ(Pillow)
+
+- https://note.nkmk.me/python-pillow-basic/
+- https://note.nkmk.me/python-pillow-imagedraw/
---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import map_gennerator
+from PIL import ImageFont, Image, ImageDraw
+
+
+def main():
+    print('>> 国土地理院のタイル画像から地図を生成 start')
+
+    # 1. タイルの番号を計算(TSで実装)
+    # 2. タイルの画像を国土地理院からダウンロード(TSで未実装)
+    # 3. 地図を合体(TSで実装)
+    # 4. 地図に線を引いて区画を区切り、番号を付与して保存
+    img = Image.open('./img/sample.png')
+    # print(img.format, img.size, img.mode)
+    # 黒字で区画ごとに線を引く
+    xBlockSize = img.size[0] // 256
+    yBlockSize = img.size[1] // 256
+    # print(xBlockSize, yBlockSize)
+
+    draw = ImageDraw.Draw(img)
+    number = 1
+    font = ImageFont.truetype('Arial.ttf', 50)
+    for y in range(yBlockSize):
+        for x in range(xBlockSize):
+            # 線を引く
+            startX = 255 * x + 1 * x
+            endX = startX + 255
+            startY = 255 * y + 1 * y
+            endY = startY + 255
+            draw.line(((startX, startY), (endX, startY),
+                       (endX, endY), (startX, endY)),
+                      fill=(0, 0, 0),
+                      width=5)
+            # 番号を付与する
+            centerX = (startX + endX) // 2 - 30
+            centerY = (startY + endY) // 2 - 30
+            draw.text((centerX, centerY), str(number), '#000000', font=font)
+            number += 1
+    img.save('./img/new.png')
+
+    # 5. それぞれの区画にgeojsonのポリゴンの線を引いて、区画ごとに保存
+    print('<< 国土地理院のタイル画像から地図を生成 done')
+
+
+if __name__ == '__main__':
+    main()
